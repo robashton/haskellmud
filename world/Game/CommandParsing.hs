bf6b0cf6bed853ef075b1205a89311de86f0c701
Rob Ashton <robashton@codeofrob.com>
2014-04-28 09:11:19 +0100

    Thinking about input structures

--- world/Game/CommandParsing.hs
+++ world/Game/CommandParsing.hs
@@ -1,5 +1,24 @@
 module Game.CommandParsing where
 
-parseCommand :: String -> String
-parseCommand a = a ++ "Parsed"
+--import Control.Applicative
+
+data Verb = Take
+          | Go
+          | Move
+          deriving Show
+
+data Direction = North
+               | West
+               | South
+               | East
+               deriving Show
+
+data Noun = Noun String
+          deriving Show
+
+data Command = Command { verb :: Verb,
+                         noun :: Noun }
+
+parseCommand :: String -> Command
+parseCommand _ = Command Take (Noun "Axe")
 
